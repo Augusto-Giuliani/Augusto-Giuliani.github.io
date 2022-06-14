2e9918f304f4323a97b9b4926ed92b491c0903a3
Pedro Civita <67804009+pedrocivita@users.noreply.github.com>
2022-06-14 17:13:22 -0300

    Mudanca das cores JS de agenda

---
+++ JS/agenda.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', function() {
+ 
+    header = document.querySelector('header')
+    header.style.backgroundColor = '#964B00'
+    h1 = document.querySelector('header h1')
+    h1.style.color = '#e6b800'
+    body = document.querySelector('body')
+    body.style.backgroundColor = '#ffe5cc'
+    ps = document.querySelectorAll('p')
+    ps[0].style.color = '#663300'
+    ps[1].style.color = '#663300'
+})
--- JS/loja.js
+++ JS/loja.js
@@ -1,18 +1,28 @@
 
 document.addEventListener('DOMContentLoaded', function() {
+
     window.onload = () => {
         var image = document.querySelectorAll('img');
-        image[2].style.height = '250px';
-        image[3].style.height = '250px';
-        image[4].style.height = '250px';
+        image[2].style.padding = '1rem';
+        image[3].style.padding = '1rem';
+        image[4].style.padding = '1rem';
+
     }
 
     window.onload = () => {
         var image = document.querySelectorAll('img');
-        image[2].style.width = '250px';
-        image[3].style.width = '250px';
-        image[4].style.width = '250px';
+        image[2].style.height = '12.5rem';
+        image[3].style.height = '12.5rem';
+        image[4].style.height = '12.5rem';
     }
+
+    window.onload = () => {
+        var image = document.querySelectorAll('img');
+        image[2].style.width = '12.5rem';
+        image[3].style.width = '12.5rem';
+        image[4].style.width = '12.5rem';
+    }
+
 
     {
     console.log(5+5)
@@ -22,6 +32,8 @@ document.addEventListener('DOMContentLoaded', function() {
     header.style.backgroundColor = '#964B00'
     h1 = document.querySelector('header h1')
     h1.style.color = '#e6b800'
+    h2 = document.querySelector('h2')
+    h2.style.padding = '1rem'
     main = document.querySelector('main')
     main.style.backgroundColor = '#ffe5cc'
     ps = document.querySelectorAll('p')
