b2a7657dc347b806d97c61799eeaf4777e8e5cb3
Pedro Civita <67804009+pedrocivita@users.noreply.github.com>
2022-06-14 16:46:40 -0300

    Js Loja

--- JS/loja.js
+++ JS/loja.js
@@ -1,18 +1,31 @@
-window.onload = () => {
-    var image = document.querySelectorAll('img');
-    image[2].style.height = '250px';
-    image[3].style.height = '250px';
-    image[4].style.height = '250px';
-}
 
-window.onload = () => {
-    var image = document.querySelectorAll('img');
-    image[2].style.width = '250px';
-    image[3].style.width = '250px';
-    image[4].style.width = '250px';
-}
+document.addEventListener('DOMContentLoaded', function() {
+    window.onload = () => {
+        var image = document.querySelectorAll('img');
+        image[2].style.height = '250px';
+        image[3].style.height = '250px';
+        image[4].style.height = '250px';
+    }
 
-{
-console.log(5+5)
-}
+    window.onload = () => {
+        var image = document.querySelectorAll('img');
+        image[2].style.width = '250px';
+        image[3].style.width = '250px';
+        image[4].style.width = '250px';
+    }
 
+    {
+    console.log(5+5)
+    }
+
+    header = document.querySelector('header')
+    header.style.backgroundColor = '#964B00'
+    h1 = document.querySelector('header h1')
+    h1.style.color = '#e6b800'
+    main = document.querySelector('main')
+    main.style.backgroundColor = '#ffe5cc'
+    ps = document.querySelectorAll('p')
+    ps[0].style.color = '#663300'
+    ps[1].style.color = '#663300'
+
+})
