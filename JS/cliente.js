59df856e12ec9117764c2798d4eb968c99e8a4a7
CaioBoa <caioob@al.insper.edu.br>
2022-06-14 23:45:37 -0300

    TERMINEI OS INPUTS

--- JS/cliente.js
+++ JS/cliente.js
@@ -1,4 +1,4 @@
-function aulas_cria()
+function proxima_pagina()
 {
     window.location.href = "diacalendario.html"
 }
@@ -9,17 +9,16 @@ function inptz()
     time_input = document.querySelector("#appt").value
     tel_input = document.querySelector("#phone").value
     service_input = document.querySelector(".service").value
-    k = [name_input,time_input,tel_input,service_input]
-    return k
+    return [name_input ,  time_input , tel_input , service_input]
 }
 
 function aulou(name_input,time_input,tel_input,service_input)
 {
-    tipin = document.querySelector(".baze").cloneNode(true)
+    tipin = document.querySelector("li").cloneNode(true)
     h2t = tipin.querySelector("h2")
     timeet = tipin.querySelector(".timet")
     cabt = tipin.querySelector(".cabelin")
-    ce2t = tipin.querySelector("celulin")
+    ce2t = tipin.querySelector(".celulin")
 
     h2t.innerHTML = name_input
     timeet.innerHTML = time_input
@@ -32,11 +31,21 @@ function aulou(name_input,time_input,tel_input,service_input)
 
 document.addEventListener('DOMContentLoaded', function() {
 
+    if (document.body.textContent.includes("Salvar")) {
     main = document.querySelector("main")
     enviar = document.querySelector(".salvar")
-    main.addEventListener('click', function(){localStorage.setItem = inptz()})
-    enviar.addEventListener('click', function(){aulas_cria()})
-
-    if (document.body.textContent.includes(""))
+    main.addEventListener('click', function(){localStorage.setItem("k1" , inptz()[0])})
+    main.addEventListener('click', function(){localStorage.setItem("k2" , inptz()[1])})
+    main.addEventListener('click', function(){localStorage.setItem("k3" , inptz()[2])})
+    main.addEventListener('click', function(){localStorage.setItem("k4" , inptz()[3])})
+    enviar.addEventListener('click', function(){proxima_pagina()})
+    }
+    else {
+    console.log(localStorage.getItem("k1"))
+    console.log(localStorage.getItem("k2"))
+    console.log(localStorage.getItem("k3"))
+    console.log(localStorage.getItem("k4"))
+    aulou(localStorage.getItem("k1"),localStorage.getItem("k2"),localStorage.getItem("k3"),localStorage.getItem("k4"))
+    }
 
 })
